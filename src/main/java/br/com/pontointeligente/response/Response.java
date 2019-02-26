332b575190aa5f112ddd7b04fb876b3d17c34cc3
Evaristo32 <evaristojr32@gmail.com>
2019-02-26 00:05:54 -0300

    Criando services

---
+++ src/main/java/br/com/pontointeligente/response/Response.java
@@ -0,0 +1,28 @@
+package br.com.pontointeligente.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Response<T> {
+
+    @Getter
+    @Setter
+    private T data;
+
+    private List<String> erros;
+
+    public List<String> getErros() {
+        if (this.erros == null){
+            this.erros = new ArrayList<>();
+        }
+        return erros;
+    }
+
+    public void setErros(List<String> erros) {
+        this.erros = erros;
+    }
+}
